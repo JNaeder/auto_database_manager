9605273b28bce3833acf792f9b168f797c7e1521
JNaeder <J.naeder324@gmail.com>
2022-08-26 19:16:27 -0400

    Added real google sheet

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import time
 class Spreadsheet:
     def __init__(self):
         self.google_sheet = gspread.oauth()
-        self.main_spreadsheet = self.google_sheet.open_by_key('1cwRxSt-tGRmwghuPMzS2DMNMjOpG9xElNuKrEtt0Eco')
-        self.sheet = self.main_spreadsheet.worksheet("All Active")
+        self.main_spreadsheet = self.google_sheet.open_by_key('1XgSe-lkrjvOb9y04XGfJ0G2n_nwLSaVFLFd9OxA-KdY')
+        self.sheet = self.main_spreadsheet.worksheet("NY GPA Dashboard PowerBI")
         self.all_values = self.sheet.get_values()[1:]
 
     def get_all_values(self):
@@ -17,9 +17,9 @@ class Spreadsheet:
 class MySQL:
     def __init__(self):
         self.database = mysql.connector.connect(
-            host="192.168.0.192",
-            # host="2.tcp.ngrok.io",
-            # port=16493,
+            # host="192.168.0.192",
+            host="2.tcp.ngrok.io",
+            port=16493,
             user="johnny",
             password="password",
             database="test_database"
@@ -32,9 +32,10 @@ class MySQL:
         return self.cursor.fetchall()
 
     def add_row_into_table(self, list_of_data):
-        sql = f"INSERT INTO all_active (student_id, student_name, mod_section, icr, gpa, email, instructor)" \
-              f"VALUES ({list_of_data[0]}, '{list_of_data[1]}', '{list_of_data[2]}'," \
-              f" {list_of_data[5]}, {list_of_data[6]}, '{list_of_data[3]}', '{list_of_data[4]}');"
+        icr_num = list_of_data[3][:-1]
+        sql = f"INSERT INTO all_active (student_id, student_name, mod_section, icr, gpa, email)" \
+              f"VALUES ({list_of_data[1]}, '{list_of_data[2]}', '{list_of_data[6]}'," \
+              f" {icr_num}, {list_of_data[4]}, '{list_of_data[1]}.us@saeinstitute.edu');"
         self.cursor.execute(sql)
         self.database.commit()
 
@@ -45,12 +46,11 @@ class MySQL:
 
     def update_student_id_db(self, spreadsheet_info):
         sql = f"UPDATE all_active SET " \
-              f"student_name='{spreadsheet_info[1]}', " \
-              f"mod_section='{spreadsheet_info[2]}', " \
-              f"icr={spreadsheet_info[5]}, " \
-              f"gpa={spreadsheet_info[6]}, " \
-              f"instructor='{spreadsheet_info[4]}' " \
-              f"WHERE student_id={spreadsheet_info[0]}"
+              f"student_name='{spreadsheet_info[2]}', " \
+              f"mod_section='{spreadsheet_info[6]}', " \
+              f"icr={spreadsheet_info[3][:-1]}, " \
+              f"gpa={spreadsheet_info[4]} " \
+              f"WHERE student_id={spreadsheet_info[1]}"
         self.cursor.execute(sql)
         self.database.commit()
 
@@ -67,15 +67,17 @@ class MasterController:
 
     @staticmethod
     def is_different_data(spreadsheet_data, db_data):
-        if spreadsheet_data[1] != db_data[1]:
+        # Check Name
+        if spreadsheet_data[2] != db_data[1]:
             return True
-        if spreadsheet_data[2] != db_data[2]:
+        # Check Mod
+        if spreadsheet_data[6] != db_data[2]:
             return True
-        if float(spreadsheet_data[5]) != db_data[3]:
+        # Check ICR
+        if float(spreadsheet_data[3][:-1]) != db_data[3]:
             return True
-        if float(spreadsheet_data[6]) != db_data[4]:
-            return True
-        if spreadsheet_data[4] != db_data[6]:
+        # Check GPA
+        if float(spreadsheet_data[4]) != db_data[4]:
             return True
         return False
 
@@ -85,21 +87,21 @@ class MasterController:
 
         for index in range(len(list_of_all_spreadsheet_data)):
             data = list_of_all_spreadsheet_data[index]
-            if int(data[0]) not in [x[0] for x in list_of_all_db_data]:
+            if int(data[1]) not in [x[0] for x in list_of_all_db_data]:
                 self.mysql.add_row_into_table(data)
-                print(f"Added {data[1]} into the table")
+                print(f"Added {data[2]} into the table")
 
         for index in range(len(list_of_all_db_data)):
             data = list_of_all_db_data[index]
-            if data[0] not in [int(x[0]) for x in list_of_all_spreadsheet_data]:
+            if data[0] not in [int(x[1]) for x in list_of_all_spreadsheet_data]:
                 self.mysql.remove_from_table(data[0])
                 print(f"Deleted {data[1]} from Table")
 
         for spreadsheet_data in list_of_all_spreadsheet_data:
-            db_data = self.mysql.get_student_info_from_student_id(spreadsheet_data[0])
+            db_data = self.mysql.get_student_info_from_student_id(spreadsheet_data[1])
             if self.is_different_data(spreadsheet_data, db_data):
                 self.mysql.update_student_id_db(spreadsheet_data)
-                print(f"Changed {spreadsheet_data[1]} Data")
+                print(f"Changed {spreadsheet_data[2]} Data")
 
 
 if __name__ == "__main__":
